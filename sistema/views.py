a2c7bef40071f1f354b4ca45fa244950dda795eb
João Gabriel <jgluciano.luz@gmail.com>
2025-07-06 16:38:24 -0300

    feat: adiciona filtro por nome de cliente e data na listagem de locações

--- sistema/views.py
+++ sistema/views.py
@@ -3,6 +3,7 @@ from .forms import LocacaoForm
 from django.contrib import messages
 from .models import Locacao
 
+
 def agendar_locacao(request):
     if request.method == 'POST':
         form = LocacaoForm(request.POST)
@@ -12,10 +13,25 @@ def agendar_locacao(request):
             return redirect('agendar_locacao')
     else:
         form = LocacaoForm()
-    
+
     return render(request, 'sistema/agendar_locacao.html', {'form': form})
-  
+
 
 def listar_locacoes(request):
-    locacoes = Locacao.objects.select_related('cliente').prefetch_related('brinquedos').order_by('-data_inicio')
-    return render(request, 'sistema/listar_locacoes.html', {'locacoes': locacoes})
+    locacoes = Locacao.objects.select_related(
+        'cliente').prefetch_related('brinquedos').order_by('-data_inicio')
+
+    nome_cliente = request.GET.get('cliente')
+    data = request.GET.get('data')
+
+    if nome_cliente:
+        locacoes = locacoes.filter(cliente__nome__icontains=nome_cliente)
+
+    if data:
+        locacoes = locacoes.filter(data_inicio=data)
+
+    return render(request, 'sistema/listar_locacoes.html', {
+        'locacoes': locacoes,
+        'nome_cliente': nome_cliente or '',
+        'data_filtro': data or '',
+    })
