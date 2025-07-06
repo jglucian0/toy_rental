ed18c5cacab5b6e27abf3c7a65c283ca8989768d
João Gabriel <jgluciano.luz@gmail.com>
2025-07-06 12:24:40 -0300

    feat: adiciona model Cliente com campos de identificação e registra no admin

--- sistema/admin.py
+++ sistema/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Brinquedo  # Importa o modelo Brinquedo
+from .models import Cliente  # Importa o modelo Cliente
 
 
 @admin.register(Brinquedo)
@@ -8,3 +9,8 @@ class BrinquedoAdmin(admin.ModelAdmin):
                     'ultima_limpeza', 'disponivel')
     list_filter = ('tipo', 'disponivel')
     search_fields = ('nome', 'tipo')
+    
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'telefone', 'cpf')
+    serch_fields = ('nome', 'telefone', 'cpf')
